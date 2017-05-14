4bef6724d52c4721cb0ab8da69c60f561122b6ed
Soojung <hong.soojung@gmail.com>
2017-05-14 23:46:10 +0200

    read data

--- Santander_Product_Recommendation.R
+++ Santander_Product_Recommendation.R
@@ -1,3 +1,7 @@
+#---------------
+# library 
+library(ggplot2)
+
 #---------------------------------------
 # Set training data and connect to it 
 # 
@@ -6,6 +10,26 @@ con = file(trainingData, "r")
 
 #----------------
 # Read 20 rows
-partialData <- read.csv(con, nrows=20)
+partialData <- read.csv(con, nrows=200)
 partialData
 str(partialData)
+
+#----------------------
+# 'age' and 'payroll' 
+data_age_payroll <- partialData[, c("ncodpers","age", "ind_nomina_ult1")]
+data_age_payroll
+
+# get the range for the x and y axis
+ageRange <- range(partialData$age)
+payrollRange <- range(partialData$ind_nomina_ult1)
+ageRange
+payrollRange
+
+# Density curve
+ggplot(data_age_payroll, aes(x=age, y=ind_nomina_ult1)) + geom_density()
+
+
+#---------------------------
+# close connection to file
+close(con)
+
